7d9da0e5520d907754cde43eb6db1dfe017df463
Jack Lu <jacklu@jacklu.me>
2022-07-17 22:04:56 +0800

    hover.ts: fix .environment type error

--- src/hover.ts
+++ src/hover.ts
@@ -1,12 +1,12 @@
 import * as vscode from 'vscode';
-import { Expression, programAt } from 'l';
+import { Expression, Statement, programAt, Annotated } from 'l';
 import documentProgram from './documentProgram';
 
 export function activateHover(context: vscode.ExtensionContext) {
 	vscode.languages.registerHoverProvider('l', {
 		provideHover(document, position, token) {
 			var program = documentProgram.get(document)!;
-			var target = programAt(program, document.offsetAt(position));
+			var target = <Annotated<Expression | Statement>>programAt(program, document.offsetAt(position));
 			if (target instanceof Expression.Name)
 				if (target.environment) {
 					var resolution = target.environment.resolve(target.identifier);
