05a09b7f783b9b0227f8d4068eaf079eda2d3861
Jack Lu <jacklu@jacklu.me>
2022-08-25 13:15:27 +0800

    +references

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,13 @@ import * as vscode from 'vscode';
 import { activateDiagnostic } from './diagnostic';
 import { activateHover } from './hover';
 import { activateDefinition } from './definition';
+import { activateReferences } from './references';
 
 export function activate(context: vscode.ExtensionContext) {
 	activateDiagnostic(context);
 	activateHover(context);
 	activateDefinition(context);
+	activateReferences(context);
 }
 
 export function deactivate() {}
---
+++ src/references.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+import { Expression, Statement, programAt, Annotated, findReferences } from 'l';
+import documentProgram from './documentProgram';
+export function activateReferences(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.languages.registerReferenceProvider('l', {
+			provideReferences(document, position, context, token) {
+				var program = documentProgram.get(document)!;
+				var target = <Annotated<Expression | Statement>>programAt(program, document.offsetAt(position));
+				if (target instanceof Expression.Name) {
+					var references = [...findReferences(target)];
+					return references.map(reference => {
+						var source = (<Annotated<Expression.Name>>reference).node.source;
+						return new vscode.Location(
+							document.uri,
+							new vscode.Range(
+								document.positionAt(source.startIdx),
+								document.positionAt(source.endIdx)
+							)
+						);
+					})
+				}
+			}
+		})
+	);
+};
