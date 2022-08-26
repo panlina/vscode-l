4bbdcf53e0553ba2d7f1adf9c04dae244ae2ab8b
Jack Lu <jacklu@jacklu.me>
2022-08-26 16:27:14 +0800

    hover: context.subscriptions.push the Disposable returned by registerHoverProvider

--- src/hover.ts
+++ src/hover.ts
@@ -3,22 +3,24 @@ import { Expression, Statement, programAt, Annotated } from 'l';
 import documentProgram from './documentProgram';
 
 export function activateHover(context: vscode.ExtensionContext) {
-	vscode.languages.registerHoverProvider('l', {
-		provideHover(document, position, token) {
-			var program = documentProgram.get(document)!;
-			var target = <Annotated<Expression | Statement>>programAt(program, document.offsetAt(position));
-			if (target instanceof Expression.Name)
-				if (target.environment) {
-					var resolution = target.environment.resolve(target.identifier);
-					if (resolution) {
-						var [type, depth] = resolution;
-						return {
-							contents: [
-								`${type}: ${target.identifier}`
-							]
-						};
+	context.subscriptions.push(
+		vscode.languages.registerHoverProvider('l', {
+			provideHover(document, position, token) {
+				var program = documentProgram.get(document)!;
+				var target = <Annotated<Expression | Statement>>programAt(program, document.offsetAt(position));
+				if (target instanceof Expression.Name)
+					if (target.environment) {
+						var resolution = target.environment.resolve(target.identifier);
+						if (resolution) {
+							var [type, depth] = resolution;
+							return {
+								contents: [
+									`${type}: ${target.identifier}`
+								]
+							};
+						}
 					}
-				}
-		}
-	});
+			}
+		})
+	);
 };
