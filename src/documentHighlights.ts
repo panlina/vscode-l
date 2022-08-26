cc601e9a4ea9567c45f8031bf77af1407c06ebdf
Jack Lu <jacklu@jacklu.me>
2022-08-26 15:21:08 +0800

    +documentHighlights

---
+++ src/documentHighlights.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+import { Expression, Statement, programAt, Annotated, findReferences } from 'l';
+import documentProgram from './documentProgram';
+export function activateDocumentHighlights(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.languages.registerDocumentHighlightProvider('l', {
+			provideDocumentHighlights(document, position, token) {
+				var program = documentProgram.get(document)!;
+				var target = <Annotated<Expression | Statement>>programAt(program, document.offsetAt(position));
+				if (target instanceof Expression.Name) {
+					var definition = target.definition || target;
+					var references = [...findReferences(definition)];
+					return [definition, ...references].map(reference => {
+						var source = (<Annotated<Expression.Name>>reference).node.source;
+						return new vscode.DocumentHighlight(
+							new vscode.Range(
+								document.positionAt(source.startIdx),
+								document.positionAt(source.endIdx)
+							)
+						);
+					});
+				}
+			}
+		})
+	);
+};
--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,14 @@ import { activateDiagnostic } from './diagnostic';
 import { activateHover } from './hover';
 import { activateDefinition } from './definition';
 import { activateReferences } from './references';
+import { activateDocumentHighlights } from './documentHighlights';
 
 export function activate(context: vscode.ExtensionContext) {
 	activateDiagnostic(context);
 	activateHover(context);
 	activateDefinition(context);
 	activateReferences(context);
+	activateDocumentHighlights(context);
 }
 
 export function deactivate() {}
