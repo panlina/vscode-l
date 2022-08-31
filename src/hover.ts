47d42ec35f017ddc691f773ac6f960be0f8a74f3
Jack Lu <jacklu@jacklu.me>
2022-08-31 21:57:25 +0800

    hover: simplify

--- src/hover.ts
+++ src/hover.ts
@@ -8,26 +8,17 @@ export function activateHover(context: vscode.ExtensionContext) {
 			provideHover(document, position, token) {
 				var program = documentProgram.get(document)!;
 				var target = <Annotated<Expression | Statement>>programAt(program, document.offsetAt(position));
-				if (target instanceof Expression.Name)
-					if (target.environment) {
-						var resolution = target.environment.resolve(target.identifier);
-						if (resolution) {
-							var [type, depth] = resolution;
-							return {
-								contents: [
-									`${type}: ${target.identifier}`
-								]
-							};
-						}
-					} else {
-						var type: "variable" | "label" =
-							target.parent instanceof Statement.Var ? 'variable' : 'label';
-						return {
-							contents: [
-								`${type}: ${target.identifier}`
-							]
-						};
-					}
+				if (target instanceof Expression.Name) {
+					var definition = 'definition' in target ? <Annotated<Expression.Name>>target.definition : target;
+					if (!definition) return;
+					var type: "variable" | "label" =
+						definition.parent instanceof Statement.Var ? 'variable' : 'label';
+					return {
+						contents: [
+							`${type}: ${target.identifier}`
+						]
+					};
+				}
 			}
 		})
 	);
